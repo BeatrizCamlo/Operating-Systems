829f2fb3f8bd2f8eb05606a96c991139888fd5d0
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-30 19:42:52 -0300

    class practice

---
+++ aula_lab/ex01.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char **argv){
+	pid_t pid=fork(); // Funcao usada para criar um novo processo
+	if(pid<0){ // Erro na criacao do processo
+		printf("Sou o processo %d e nao consegui criar um novo processo.\n",getpid());
+		return 1;
+		} 
+		else if (pid==0){ // Processo filho
+			fork();
+			fork();
+			fork();
+			fork();
+			// printf("Sou o processo filho, meu PID e %d.\n",getpid());
+		}else{// Processo pai
+			wait(NULL); // Funcao que faz o processo pai esperar pelo final da execucao de um de seus filhos
+			printf("Sou o processo pai do %d, meu PID e %d.\n",pid,getpid());
+		}
+	return 0;
+	}	
