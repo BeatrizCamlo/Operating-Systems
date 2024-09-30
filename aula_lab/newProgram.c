3af512da036d4b271bfad7478c3ddd0970e7991f
beatrizcamilo <abeatrizcamilo@gmail.com>
2024-09-30 19:44:00 -0300

    class practice

---
+++ aula_lab/newProgram.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char **argv){
+
+	printf("Sou o processo %d e estou executando o meu proprio programa.\n",getpid());
+	sleep(10); //Função que faz o programa dormir durante 10 segundos
+	
+	
+	}
